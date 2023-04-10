48d036b437c6b1420ddab5754bb78c3eab1d8a16
Talha Razzaq <tctnpk@gmail.com>
2023-04-10 20:32:47 +0500

    hashrouter added

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import './App.css';
 import Navbar from './components/Navbar';
 import PasswordGenerator from './components/Qrpass';
 import TextToQR from './components/TextToQR';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Navbar />
         <Routes>
           <Route path='/' element={<PasswordGenerator />} />
           <Route path='/texttoqr' element={<TextToQR />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,40 +29,6 @@ export default function Navbar() {
                                 Text to QR
                             </Link>
                         </li>
-                        <li className="nav-item dropdown">
-                            <a
-                                className="nav-link dropdown-toggle"
-                                href="/"
-                                role="button"
-                                data-bs-toggle="dropdown"
-                                aria-expanded="false"
-                            >
-                                Dropdown
-                            </a>
-                            <ul className="dropdown-menu">
-                                <li>
-                                    <a className="dropdown-item" href="/">
-                                        Action
-                                    </a>
-                                </li>
-                                <li>
-                                    <a className="dropdown-item" href="/">
-                                        Another action
-                                    </a>
-                                </li>
-                                <li>
-                                    <hr className="dropdown-divider" />
-                                </li>
-                                <li>
-                                    <a className="dropdown-item" href="/">
-                                        Something else here
-                                    </a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li className="nav-item">
-                            <a href="/" className="nav-link disabled">Disabled</a>
-                        </li>
                     </ul>
                     <form className="d-flex" role="search">
                         <input
@@ -71,7 +37,7 @@ export default function Navbar() {
                             placeholder="Search"
                             aria-label="Search"
                         />
-                        <button onClick={(e)=>e.preventDefault()} className="btn btn-outline-success" type="submit">
+                        <button onClick={(e) => e.preventDefault()} className="btn btn-outline-success" type="submit">
                             Search
                         </button>
                     </form>
